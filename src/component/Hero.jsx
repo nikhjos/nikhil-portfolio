f2dbaf9f4df49c3ea8aa976b0358ee9d611be08f
unknown <webdevdes12@gmail.com>
2024-06-12 01:27:55 -0700

    new hero design

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -8,6 +8,9 @@ const Hero = () => {
     <section>
       <div id="hero">
         <div className="hero-center">
+          <div className="hero-image-small">
+            <img src={heroImg} alt="hero" />
+          </div>
           <div className="hero-intro">
             <h4>Hello There, My Name is</h4>
             <h2>Nikhil Joshi</h2>
